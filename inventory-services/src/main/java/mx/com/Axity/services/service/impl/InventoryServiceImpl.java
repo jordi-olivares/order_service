c749abc9071e43753a463707bf8d9f05665765c5
jordi-olivares <jordicuenta2@gmail.com>
2022-09-11 01:19:09 -0500

    funciona con la base de datos

--- inventory-services/src/main/java/mx/com/Axity/services/service/impl/InventoryServiceImpl.java
+++ inventory-services/src/main/java/mx/com/Axity/services/service/impl/InventoryServiceImpl.java
@@ -28,7 +28,7 @@ public class InventoryServiceImpl implements IInventoryService {
     public List<UserTO> getUsers() {
 
         LOG.info("Consultando usuarios");
-        List<UserDO> usersDO = this.userDAO.findByLastName("Rodriguez");
+        List<UserDO> usersDO = this.userDAO.findByLastName("Romero");
 
         Type userDAOType = new TypeToken<List<UserDO>>() {}.getType();
         List<UserTO> usersTO = this.modelMapper.map(usersDO, userDAOType);
